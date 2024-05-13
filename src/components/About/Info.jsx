17447bfdc216ca23f25052a663c7ddf7e28a8578
avinashvishu <loginavi1@gmail.com>
2024-05-14 00:30:12 +0530

    projects

--- src/components/About/Info.jsx
+++ src/components/About/Info.jsx
@@ -11,7 +11,7 @@ const Info = () => {
       <div className="about__box">
         <i class="bx bx-briefcase-alt about__icon"></i>
         <h3 className="about__title">Completed</h3>
-        <span className="about__subtitle">30+ projects</span>
+        <span className="about__subtitle">10+ projects</span>
       </div>
       <div className="about__box">
         <i class="bx bx-support about__icon"></i>
